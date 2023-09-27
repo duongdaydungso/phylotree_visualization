b0466f414a63ce9040df88ded8c5eccd5e4558af
ThaiDuong <83547027+duongdaydungso@users.noreply.github.com>
2023-09-27 12:40:33 +0700

    Upgrade zooming lib

--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -1,5 +1,4 @@
 declare module "phylotree";
-declare module "react-zoom-pan-pinch";
 declare module "d3-react-axis";
 declare module "*.png";
 /// <reference path="modules/save-svg-as-png/index.d.ts" />
